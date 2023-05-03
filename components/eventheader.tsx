9338ff65b71f929215dfceabe795b796b3c9d57c
mustythefirst <m.adde.osman@gmail.com>
2023-05-03 20:45:30 +0100

    https://github.com/mustythefirst/ticketchain.git

---
+++ components/eventheader.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+function Eventheader() {
+  return (
+    <>
+      <div><h4>Events</h4></div>
+    </>
+  );
+}
+
+export default Eventheader;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,69 @@
 import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
 import type { AppProps } from 'next/app';
-import { configureChains, createClient, WagmiConfig } from 'wagmi';
-import { arbitrum, goerli, mainnet, optimism, polygon } from 'wagmi/chains';
+import {
+  RainbowKitProvider,
+  getDefaultWallets,
+  connectorsForWallets,
+} from '@rainbow-me/rainbowkit';
+import { argentWallet, trustWallet } from '@rainbow-me/rainbowkit/wallets';
+import { createClient, configureChains, WagmiConfig } from 'wagmi';
+import { Chain } from 'wagmi/chains';
 import { publicProvider } from 'wagmi/providers/public';
 
+const sepolia: Chain = {
+  id: 11155111,
+  name: 'Sepolia',
+  network: 'sepolia',
+  nativeCurrency: { name: 'Sepolia Ether', symbol: 'ETH', decimals: 18 },
+  rpcUrls: {
+    alchemy: { http: ['https://eth-sepolia.g.alchemy.com/v2/m-ENVUngFO4we3LF-C5-Lrp3ZqmMgCAO'] },
+    default: { http: ['https://eth-sepolia.g.alchemy.com/v2/m-ENVUngFO4we3LF-C5-Lrp3ZqmMgCAO'] },
+    public: { http: ['https://rpc.ankr.com/eth_rinkeby'] },
+  },
+  blockExplorers: {
+    etherscan: { name: 'Etherscan', url: 'https://sepolia.etherscan.io/' },
+    default: { name: 'Etherscan', url: 'https://sepolia.etherscan.io/' },
+  },
+  contracts: {
+    ensRegistry: {
+      address: '0x8aBd25fa3226533bAb1bcFDF7877576e703f0A0F',
+    },
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 10299530,
+    },
+  },
+  testnet: true,
+};
+
 const { chains, provider, webSocketProvider } = configureChains(
-  [
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [goerli] : []),
-  ],
+  [sepolia],
   [publicProvider()]
 );
 
-const { connectors } = getDefaultWallets({
-  appName: 'RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
+const projectId = 'YOUR_PROJECT_ID';
+
+const { wallets } = getDefaultWallets({
+  appName: 'RainbowKit Mint NFT Demo',
+  projectId,
   chains,
 });
+
+const demoAppInfo = {
+  appName: 'RainbowKit Mint NFT Demo',
+};
+
+const connectors = connectorsForWallets([
+  ...wallets,
+  {
+    groupName: 'Other',
+    wallets: [
+      argentWallet({ projectId, chains }),
+      trustWallet({ projectId, chains }),
+    ],
+  },
+]);
 
 const wagmiClient = createClient({
   autoConnect: true,
@@ -33,7 +75,7 @@ const wagmiClient = createClient({
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains}>
+      <RainbowKitProvider appInfo={demoAppInfo} chains={chains}>
         <Component {...pageProps} />
       </RainbowKitProvider>
     </WagmiConfig>
